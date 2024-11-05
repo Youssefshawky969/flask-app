498b1c47e3a39e43233f0870a3dde4cc99b94236
Youssefshawky969 <168371773+Youssefshawky969@users.noreply.github.com>
2024-11-05 22:32:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
 from flask import Flask
-from prometheus_client import Counter, Histogram, generate_latest
 
-#This creates an instance of the Flask class, representing your web application.
 app = Flask(__name__)
 
-REQUEST_COUNT = Counter('app_request_count', 'Total request count')
-REQUEST_LATENCY = Histogram('app_request_latency_seconds', 'Request latency')
-
-#This is a route decorator that defines the URL path. In this case, it’s the homepage (/).
 @app.route('/')
-
-# This function is executed when the homepage is accessed and returns a simple message.
 def home():
-    REQUEST_COUNT.inc()
-    with REQUEST_LATENCY.time():
-        return "Hello, World! My name is Youssef Shawky and this is a simple python web app Automated by Jenkins."
+    return "Hello, World! This is a Dockerized Python app."
 
-@app.route('/metrics')
-def metrics():
-    return generate_latest()
-
-if __name__ == '__main__':  #a common Python idiom used to control the behavior of a script when it's run directly.
-    app.run(host='0.0.0.0' , port=5000)  #This starts the web server and makes the app accessible, host='0.0.0.0' ensures it can be accessed externally, and port=5000 sets the port.
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
